c22d76be49721d0afafeb3d6bb07e02a1d41d098
Fernando <fernando_0970@outlook.es>
2019-08-14 00:25:30 -0500

    Loader Spinner added. Loader while waiting results

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 const state ={}
 
@@ -13,13 +13,15 @@ const controlSearch = async() => {
 		state.search = new Search(query);
 
 		// 3.- Prepare UI for results
-		searchView.clearInput()
-		searchView.clearResults()
+		searchView.clearInput();
+		searchView.clearResults();
+		renderLoader(elements.searchRes); 
 
 		// 4.- Search for recipes
 		await state.search.getResults();
 
 		// 5.- Render results to UI
+		clearLoader();
 		searchView.renderResults(state.search.result);
 	}
 }
