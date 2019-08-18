ccb78729371336a471ac0abe8bdae0c000694d26
Fernando <fernando_0970@outlook.es>
2019-08-18 12:42:15 -0500

    Shopping controller finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 const state ={}
-
+window.state = state;
 // SEARCH CTRL
 
 const controlSearch = async() => {
@@ -91,6 +93,38 @@ const ctrlRecipe = async() => {
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, ctrlRecipe));
 
+// LIST CTRL
+
+const controlList = () => {
+	// Create a list obj if there is not
+	if (!state.list) state.list = new List();
+
+	// Add ingredient to the list and UI
+	state.recipe.ingredients.forEach(el => {
+		const item = state.list.addItem(el.count, el.unit, el.ingredient);
+		listView.renderItem(item);
+	});
+}
+
+// Handling delete and update list item events
+elements.shopping.addEventListener('click', e => {
+	const id = e.target.closest('.shopping__item').dataset.itemid;
+
+	// Handle the delete button
+	if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+		// Delete from state
+		state.list.deleteItem(id);
+
+		// Delete from UI
+		listView.deleteItem(id);
+		
+	} else if (e.target.matches('.shopping__count-value')) {
+		const val = parseFloat(e.target.value, 10);
+		state.list.updateCount(id, val);
+
+	}
+});
+
 // Handling recipe buttons clicks
 elements.recipe.addEventListener('click', e => {
 	if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -103,6 +137,8 @@ elements.recipe.addEventListener('click', e => {
 		// Increase button gets clicked
 		state.recipe.updateServings('inc');
 		recipeView.updateServingIngredients(state.recipe);
+	} else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+		controlList();
 	}
-	console.log(state.recipe);
 });
+
